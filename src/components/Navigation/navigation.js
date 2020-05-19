d1752d6b1dc23a646fc05455c8207523375a3c89
Thomas Stagl <t.stagl@durchblicker.at>
2020-05-19 16:54:15 +0200

    fixed the navigation toggling

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import NavItem from './navItem'
-import DesktopMenu from './desktopMenu'
-import MobileMenu from './mobileMenu'
 
 const links = [
   { to: '/', name: 'Start' },
@@ -15,12 +13,29 @@ const navItems = links.map((item, index) => (
 ))
 
 const Navigation = () => {
-  
+  const [burgerMenuVisible, setBurgerMenuVisible] = React.useState(false)
   return (
     <div className="absolute right-0">
       <nav className="p-4 my-4">
-        <DesktopMenu items={navItems}/>
-        <MobileMenu items={navItems}/>
+        <div className="lg:hidden" id="mobile-nav-toggle">
+          <button
+            id="toggle-burger-menu"
+            onClick={() => setBurgerMenuVisible(!burgerMenuVisible)}
+          >
+            <svg
+              className="fill-current h-6 w-6 "
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+          </button>
+        </div>
+
+        <div className={`${!burgerMenuVisible ? 'hidden' : ''} lg:block`} id="desktop-menu">
+          <ul className="flex justify-center">{navItems}</ul>
+        </div>
       </nav>
     </div>
   )
