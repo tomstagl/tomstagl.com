002813d66dec7be6709013b31ad868b38faf9043
Thomas Stagl <t.stagl@durchblicker.at>
2020-05-20 09:59:19 +0200

    make logo clickable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import Avatar from './avatar'
@@ -6,11 +5,8 @@ import Navigation from './Navigation/navigation'
 
 const Header = ({ siteTitle }) => (
   <header className="pl-4 mx-auto sm:max-w-l md:max-w-lg lg:max-w-3xl">
-    <div className="relative flex flex-row items-center">
-      <Avatar />
-      <div className="justify-start font-extrabold text-2xl">
-        <Link to="/">{siteTitle}</Link>
-      </div>
+    <div className="w-full container mx-auto justify-between iiii relative flex flex-wrap items-center">
+      <Avatar title={siteTitle} />
       <Navigation />
     </div>
   </header>
--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
-const Avatar = () => {
+const Avatar = ({ title }) => {
   const data = useStaticQuery(graphql`
     query {
       avatarImage: file(relativePath: { eq: "avatar.png" }) {
@@ -16,13 +17,18 @@ const Avatar = () => {
   `)
 
   return (
-    <div className="justify-start">
-      <Img
-        className="h-16 w-16 mr-4 rounded-full mx-auto border-2 border-teal-400"
-        fluid={data.avatarImage.childImageSharp.fluid}
-        alt="Portrait of Tom Stagl"
-      />
-    </div>
+    <>
+      <div>
+        <Link to="/" className="flex flex-auto items-center text-2xl">
+          <Img
+            className="h-16 w-16 mr-4 rounded-full mx-auto border-2 border-teal-400"
+            fluid={data.avatarImage.childImageSharp.fluid}
+            alt="Portrait of Tom Stagl"
+          />
+          <div>{title}</div>
+        </Link>
+      </div>
+    </>
   )
 }
 
