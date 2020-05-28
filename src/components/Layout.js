5691c4583a73a5a0dc337c8eb1ac8a1ab884bfa4
Thomas Stagl <t.stagl@durchblicker.at>
2020-05-28 08:20:53 +0200

    add twitter and linkedin links to the footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
+import { FaTwitter, FaLinkedin } from 'react-icons/fa'
 
 import Header from './header'
 import './layout.css'
@@ -21,8 +22,33 @@ const Layout = ({ children }) => {
     <div className="mx-auto">
       <Header siteTitle="Tom Stagl" />
       <main className="p-4 max-w-2xl mx-auto">{children}</main>
-      <footer className="p-4 w-12/12 max-w-none text-xs text-center">
-        <Link to="/impressum">Imprint</Link>
+      <footer className="p-4 mx-auto max-w-2xl content-start text-center">
+        <div className="flex flex-row">
+          <div className="w-1/3"></div>
+          <div className="w-1/3">
+            <Link to="/impressum" className="text-xs">
+              Imprint
+            </Link>
+          </div>
+          <div className="w-1/3 flex flex-row-reverse">
+            <a
+              className="px-4"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://twitter.com/herrstagl"
+            >
+              <FaTwitter className="text-teal-600" />
+            </a>
+
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://at.linkedin.com/in/herrstagl"
+            >
+              <FaLinkedin className="text-teal-600" />
+            </a>
+          </div>
+        </div>
       </footer>
     </div>
   )
