bb713ec8164a9c93acfe5e9281e8dec7e7d7dd2d
Thomas Stagl <t.stagl@durchblicker.at>
2020-06-03 09:38:35 +0200

    change the title for the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Business Agility Coach" />
     <div>
       <h1>👋 I&apos;m Tom Stagl</h1>
       <p>
