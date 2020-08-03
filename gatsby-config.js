b44f9539285368d2f5154dee663ddf81dca49815
Thomas Stagl <thomas.stagl@gmail.com>
2020-08-04 00:20:04 +0200

    add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://tomstagl.com`,
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
