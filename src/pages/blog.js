7c6c9c94fc418f87123d0633475ecf94fac73fa8
Thomas Stagl <thomas.stagl@gmail.com>
2020-09-16 07:57:46 +0200

    dynamic set last post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,18 +6,30 @@ import BlogPost from '../components/Blog/BlogList/blogPost'
 import SEO from '../components/seo'
 
 const Blog = (data) => {
+  let lastPost
   const blogData = data.data.allDatoCmsBlogpost.edges
+  const firstPostData = blogData.shift()
   const firstPost = (
-    <BlogPost key="firstPost" post={blogData[0].node} latest={true} />
+    <BlogPost key="firstPost" post={firstPostData.node} latest={true} />
   )
+  if (blogData.length % 2 === 1) {
+    const lastPostData = blogData.pop()
+    lastPost = (
+      <BlogPost
+        key="lastPost"
+        post={lastPostData.node}
+        latest={false}
+        last={true}
+      />
+    )
+  }
 
   const blogPosts = (blogItems) => {
     const items = []
-    for (let iter = 1; iter < blogItems.length; iter += 2) {
+    for (let iter = 0; iter < blogItems.length; iter += 2) {
       items.push(
         <div className="md:flex">
           <BlogPost post={blogItems[iter].node} className="pr-2" />
-
           {iter + 1 < blogItems.length && (
             <BlogPost post={blogItems[iter + 1].node} className="pl-2" />
           )}
@@ -37,6 +49,7 @@ const Blog = (data) => {
           {firstPost}
           <h2>Older Posts</h2>
           {blogPosts(blogData)}
+          {lastPost && lastPost}
         </div>
       </div>
       <div className="pt-4">
@@ -57,7 +70,7 @@ export const query = graphql`
   {
     allDatoCmsBlogpost(
       filter: { meta: { status: { eq: "published" } } }
-      sort: { fields: meta___createdAt, order: DESC }
+      sort: { fields: meta___publishedAt, order: DESC }
     ) {
       edges {
         node {
@@ -69,7 +82,7 @@ export const query = graphql`
             publishedAt(formatString: "DD. MMM YYYY")
           }
           blogimage {
-            fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+            fluid(maxWidth: 800, imgixParams: { fm: "jpg", auto: "compress" }) {
               ...GatsbyDatoCmsFluid
             }
           }
