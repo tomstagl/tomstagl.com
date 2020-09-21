20e354593214f79b80a6526bb8d5799508c92e6d
Thomas Stagl <thomas.stagl@gmail.com>
2020-09-21 11:34:02 +0200

    changes

--- src/components/Footer/ShareArticle/shareArticle.js
+++ src/components/Footer/ShareArticle/shareArticle.js
@@ -3,9 +3,8 @@ import { FaTwitter, FaLinkedin } from 'react-icons/fa'
 
 const ShareArticle = () => {
   return (
-    <div className="flex justify-end items-center">
+    <div className="flex justify-end items-center border-t-2 border-gray-300 pt-2">
       <div className="border-gray-800 border-t-4 ml-auto align-middle"> </div>
-      <span className="text-sm text-gray-600 font-semibold">Share article</span>
       <a
         aria-label="twitter"
         className="px-4"
