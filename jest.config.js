4df1c00e9ebd4f58fb71ba9c7973f8ec4e9771b8
Thomas Stagl <thomas.stagl@gmail.com>
2020-10-13 16:41:02 +0200

    pff

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   transform: {
     '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
+    '^.+\\.ts?$': 'ts-jest',
   },
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
