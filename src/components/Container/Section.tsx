131320b8c19872e6586d2209bb75d0b6334bc8b5
Thomas Stagl <thomas.stagl@gmail.com>
2020-10-15 09:52:29 +0200

    add usage of container class

--- src/components/Container/Section.tsx
+++ src/components/Container/Section.tsx
@@ -15,7 +15,7 @@ const Section: FunctionComponent<SectionProps> = ({
   const darkerBackground = dark ? 'bg-gray-100 md:border' : ''
   return (
     <section className={`py-4 lg:py-12 ${css} ${darkerBackground}`}>
-      <div className="max-w-xl md:max-w-3xl lg:max-w-4xl mx-auto">
+      <div className="container mx-auto max-w-xl md:max-w-3xl lg:max-w-4xl">
         {children}
       </div>
     </section>
