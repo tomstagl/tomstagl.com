36b88fa85a76a37560569e0e2556e16798de77ea
Thomas Stagl <thomas.stagl@gmail.com>
2020-10-15 15:41:29 +0200

    fix purging of css

--- src/components/Avatar/avatar.tsx
+++ src/components/Avatar/avatar.tsx
@@ -20,30 +20,36 @@ export const PureAvatar: FunctionComponent<AvatarProps> = ({
   data,
   avatarSize = 16,
   className = 'mr-6',
-}) => (
-  <div className={`h-${avatarSize}`}>
-    <div className={`flex items-center flex-shrink-0 text-white ${className}`}>
-      <Link
-        to="/"
-        className="flex flex-row items-center text-2xl text-gray-400"
+}) => {
+  const avatarSizeHeight = `h-${avatarSize}`
+  const avatarSizeWidth = `w-${avatarSize}`
+  return (
+    <div className={avatarSizeHeight}>
+      <div
+        className={`flex items-center flex-shrink-0 text-white ${className}`}
       >
-        <Img
-          className={`h-${avatarSize} w-${avatarSize} mr-4 rounded-full mx-auto border-2 border-teal-400 bg-white`}
-          fluid={data.avatarImage.childImageSharp.fluid}
-          alt="Portrait of Tom Stagl"
-        />
-        {title && (
-          <span
-            className="font-semibold text-xl tracking-tight"
-            data-testid="avatar-title"
-          >
-            {title}
-          </span>
-        )}
-      </Link>
+        <Link
+          to="/"
+          className="flex flex-row items-center text-2xl text-gray-400"
+        >
+          <Img
+            className={`${avatarSizeHeight} ${avatarSizeWidth} mr-4 rounded-full mx-auto border-2 border-teal-400 bg-white`}
+            fluid={data.avatarImage.childImageSharp.fluid}
+            alt="Portrait of Tom Stagl"
+          />
+          {title && (
+            <span
+              className="font-semibold text-xl tracking-tight"
+              data-testid="avatar-title"
+            >
+              {title}
+            </span>
+          )}
+        </Link>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 const Avatar: FunctionComponent<AvatarProps> = (props) => {
   const { title, ...rest } = props
